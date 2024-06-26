e7464e3943f21e2ed13c2a61a06f1a6902596ba8
abdullayevazulya <zulyabdullayevaa@gmail.com>
2024-06-26 17:27:26 +0300

    add Task array

---
+++ Task-09 Array-part-1/app.js
@@ -0,0 +1,26 @@
+// // Task1
+// const names =["Aydan","Gülnaz","Gunel"]
+//  names.push("Lale")
+//  names.shift("Aydan")
+// console.log(names);
+
+
+// // Task2
+// const firstname =["Aydan","Gülnaz","Gunel"] metod1
+// firstname[2]="Classified";
+// console.log(firstname);
+
+// const firstname=["Aydan","Gülnaz","Gunel"] metod2
+// firstname.splice(2,2,"Classified")
+// console.log(firstname);
+
+
+// Task3
+// const numbers=[1,2,3,4,5];
+// const numbers2 = numbers.concat();
+// for (let i = 0; i < numbers2.length; i++) {
+//     numbers2[i] *= 2;}
+//     console.log(numbers);
+//     console.log(numbers2);
+
+
