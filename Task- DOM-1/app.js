8ef0657b29cadd191ae5b33fc2bf176fec969ec4
abdullayevazulya <zulyabdullayevaa@gmail.com>
2024-07-03 01:54:54 +0300

    add DOM-Part1

---
+++ Task- DOM-1/app.js
@@ -0,0 +1,23 @@
+
+// TASK1 
+// console.dir(document.body.children[1].firstElementChild.textContent) 
+ 
+ 
+// TASK2 
+// document.body.children[0].remove() 
+// console.dir(document.body.children[0].textContent) 
+ 
+ 
+// document.body.children[0].appendChild(document.createElement("li")).textContent =   "TITAN RTX" 
+ 
+// console.dir(document.body.children[0].textContent) 
+ 
+ 
+// TASK3 
+// // console.dir(document.body.children[0].innerHTML="GeForce 20 Series") 
+// console.dir (document.body.children[1].lastElementChild.style.backgroundColor ="#ebebeb") 
+// console.dir (document.body.children[1].children[2].style.backgroundColor ="#ebebeb")
+
+
+// TASK4
+//Siyahının primary klasına sahib olan bütün elementlərini konsola çıxarın.
