6992722ca143355af58c7efc7cf49f33b2cd46a6
abdullayevazulya <zulyabdullayevaa@gmail.com>
2024-07-05 09:52:33 +0300

    add DOM-2

---
+++ Task-DOM-2/app.js
@@ -0,0 +1,24 @@
+
+let body=document.body
+
+
+let grey = document.querySelector(".grey") 
+ 
+let red = document.querySelector(".red") 
+ 
+let blue = document.querySelector(".blue") 
+ 
+let yellow = document.querySelector(".yellow") 
+ 
+grey.onclick = () => { 
+   body.style.backgroundColor = "grey" 
+} 
+red.onclick = () => { 
+   body.style.backgroundColor = "red" 
+} 
+blue.onclick = () => { 
+   body.style.backgroundColor = "blue" 
+} 
+yellow.onclick = () => { 
+   body.style.backgroundColor = "yellow" 
+}
