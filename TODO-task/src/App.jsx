5345eb7b3f0ad92104e3d87319228d34092b5142
abdullayevazulya <zulyabdullayevaa@gmail.com>
2024-08-12 18:25:49 +0400

    add first commit

---
+++ TODO-task/src/App.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import "./App.css";
+import TodoForm from "./components/Form";
+import TodoItem from "./components/Todo/todo";
+
+function App() {
+  const [todos, setTodos] = useState([]);
+  const addTask = (userInput) => {
+    if (userInput) {
+      const newTask = {
+        id: Date.now(),
+        task: userInput,
+        completed: false,
+      };
+      setTodos([...todos, newTask]);
+    }
+  };
+  const removeTask = (id) => {
+    setTodos([...todos.filter((todo) => todo.id !== id)]);
+  };
+
+  const toggleTask = (id) => {
+    setTodos([
+      ...todos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : { ...todo }
+      ),
+    ]);
+  };
+  return (
+    <div className="todo-app">
+      <h1>Todo List</h1>
+      <TodoForm addTask={addTask} />
+      <hr className="separator" />
+      {todos.map((todo) => (
+        <TodoItem
+          todo={todo}
+          key={todo.id}
+          removeTask={removeTask}
+          toggleTask={toggleTask}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default App;
---
+++ TODO-task/src/components/Form/index.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+
+const TodoForm =({addTask})=>{
+const [userInput,setUserInput]=useState("")
+const handleChange =(event)=>{
+    setUserInput(event.target.value);
+}
+
+const hanleSubmit=(event)=>{
+    event.preventDefault()
+    addTask(userInput)
+    setUserInput("")
+}
+
+
+  return (
+    <form  className='todo-form'
+    onSubmit={hanleSubmit}> 
+    <input className='todo-input'
+     type="text" 
+    placeholder='Add a new task...' 
+    onChange={handleChange} 
+    value={userInput}/>
+     <button className='todo-button'>Save</button>
+    </form>
+   
+  );
+}
+
+
+export default TodoForm;
